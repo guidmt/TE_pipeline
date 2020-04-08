98ce7fe3b5da22a1880b062ccc90619fec6a3dc7
Guidantonio <guim86@gmail.com>
2020-04-08 20:27:42 +0100

    update 07/04/2020
    - add bpipe script
    - remove VariableLength.pl
    - add VariableLength.new.pl

--- utils/fasta_reference_portion_VariableLength_new.pl
+++ utils/fasta_reference_portion_VariableLength_new.pl
@@ -4,18 +4,16 @@ use strict;
 
 # ARGV[0]=poslist (tab separated chr\tpos\tlength\n)
 # ARGV[1]=output name.fasta
-# ARGV[2]=casual number, not important
-# ARGV[3]= up or down
+# ARGV[2]= up or down
+
 my $i;
-my$j;
-my$k;
-#my $chr=$ARGV[1];
+my $j;
+my $k;
+
 my @chrlines;
-#my $chrlines;
 my $string="";
-#my $out=$chr."recAfterAncestral.txt";
-#my $anc=$chr."ancestralAllele.txt";
-#open (ANC, ">$anc");
+my $length_var="";
+
 open (OUT, ">$ARGV[1]");
 my $chr="-999";
 my $pos;
@@ -43,22 +41,23 @@ for $i (0 .. $#snplines){
     #close (CHR);
     #@chrlines= ();
 	#$chr= $splitsnp[2];
-    $ARGV[2]=$splitsnp[2];
+    $length_var=$splitsnp[2];
     #print STDERR "For $splitsnp[0] and $splitsnp[1] my Distance is $ARGV[2]!\n";
-    if ($ARGV[3] eq "up"){
-    	 $pos= ($splitsnp[1] - 1 - $ARGV[2]);
+    if ($ARGV[2] eq "up"){
+    	 $pos= ($splitsnp[1] - 1 - $length_var);
 	}
 
- if ($ARGV[3] eq "down"){
+ if ($ARGV[2] eq "down"){
          $pos= ($splitsnp[1] - 1 );
         }
 #print STDERR "your pos is $splitsnp[1] and my pos is $pos\n";
-my $end= $pos + $ARGV[2];
-    my $base= uc(substr $string, $pos, $ARGV[2]);
+my $end= $pos + $length_var;
+    my $base= uc(substr $string, $pos, $length_var);
     #if ($base eq "N" or $base eq "-" or $base eq "."){
 # next;
     #}
 my $h=">".$splitsnp[0].":".($pos+1)."-".$end;
+
 print OUT "$h\n$base\n";
     #print ANC "$splitsnp[0]\t$base\n";
     #print OUT "$splitsnp[1]\t$base\n";
